7032f13662097b8f6a481d554b562f15ec25a6bf
Gman <gonmart@gmail.com>
2018-05-17 16:42:46 -0700

    New Test2

---
+++ Test2/App.js
@@ -0,0 +1,10 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    launch: function() {
+        //Write app code here
+        console.log("THis is my new App");
+        console.log("and I'm like crazy running this");
+        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+    }
+});
