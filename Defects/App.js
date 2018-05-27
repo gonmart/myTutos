1ba6ff637db91d497cdcaf64cf52a6bfebb81474
Gonzalo <gonmart@gmail.com>
2018-05-27 01:27:01 -0700

    Updates from Office Friday

--- Defects/App.js
+++ Defects/App.js
@@ -17,6 +17,54 @@ Ext.define('CustomApp', {
     myGrid: undefined,
 
     launch: function() {
-        
-    }
+        this._loadMilestones();
+    },
+
+
+    _loadMilestones: function(){
+        var mileCombobox = Ext.create('Rally.ui.combobox.MilestoneComboBox', {
+            itemId: 'milestone-combobox',
+            fieldLabel: 'Milestone: ',
+            labelAlign: 'right',
+            listeners: {
+                ready: this._loadDefectSuites,
+                select: this._loadData,
+                scope: this
+            }
+        });        
+
+        this.down("#pulldown-container").add(mileCombobox);
+    },
+
+
+    _loadDefectSuites: function() {
+        var dsuiteCombobox = Ext.create ('Rally.ui.combobox.ArtifactSearchComboBox', {
+            itemId: 'defectsuite-combobox',
+            fieldLabel: 'Defect Suite: ',
+            labelAlign: 'right',
+            storeConfig: {
+                autoload: true, 
+                models: ['DefectSuite'],
+                sorters: {
+                    property: 'FormattedID',
+                    direction: 'DESC'
+                }
+            },
+            listeners: {
+                ready: this._loadData,
+                select: this._loadData,
+                scope: this
+            }
+        });
+
+        this.down('#pulldown-container').add(dsuiteCombobox);
+    },
+
+
+    _loadData: function(){
+        console.log('Loading data...');
+    },
+
+
+
 });
